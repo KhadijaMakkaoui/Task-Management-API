50cdc67101d64e107078c81e2f1b851672940299
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2024-10-05 20:44:04 +0100

    create TaskSerializer

--- task_management/tasks/serializers.py
+++ task_management/tasks/serializers.py
@@ -6,3 +6,10 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email']
+    
+class TaskSerializer(serializers.ModelSerializer):
+    # Assign automatically based on the logged-in user
+    user = UserSerializer(read_only=True)  
+    class Meta:
+        model = Task
+        fields = ['id', 'title', 'description', 'due_date', 'priority_level', 'status', 'user', 'completed_at']
