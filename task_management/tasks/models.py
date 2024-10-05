ab2ccc127510aa89feb9a779b2918c7ca7a73342
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2024-10-05 20:20:51 +0100

    create task model's attributes

--- task_management/tasks/models.py
+++ task_management/tasks/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
+class Task(models.Model):
+    PRIORITY_CHOICES=[
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High'),
+    ]
+    
+    STATUS_CHOICES =[
+        ('pending', 'Pending'),
+        ('completed', 'Completed'),
+    ]
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    due_date = models.DateField()
+    priority_level= models.CharField(max_length=10, choices=PRIORITY_CHOICES)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+    completed_at = models.DateTimeField(null=True, blank=True)
+    
+
+    
+    
+    
