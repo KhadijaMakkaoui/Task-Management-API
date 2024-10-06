40329518a323b5d4242f5281a7db7b727bfaf978
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2024-10-06 15:05:58 +0100

    resolve error

--- task_management/tasks/urls.py
+++ task_management/tasks/urls.py
@@ -2,10 +2,12 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import TaskViewSet, UserViewSet
 
+# Create a router and register the viewsets
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet, basename='task')
 router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
+    # This includes all CRUD URLs for tasks and users
     path('', include(router.urls)),
 ]
