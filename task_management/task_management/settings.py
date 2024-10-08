00bda83ebd06ed3553812a64e6223ceeaac94d63
Khadija Makkaoui <makkaoui.khadija44@gmail.com>
2024-10-08 09:26:41 +0100

    install django filter and update settings to include DjangoFilterBackend

--- task_management/task_management/settings.py
+++ task_management/task_management/settings.py
@@ -47,6 +47,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',  # By default, require users to be logged in
     ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter',
+    ],
 }
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
